0edff373109c6e075a822bdb5e0d8e54c018ee99
dienamo <46058257+dienamo@users.noreply.github.com>
2022-09-01 15:19:11 +0200

    modification du service export par un service custom au lieu de mongoose

--- src/services/exports/exports.service.ts
+++ src/services/exports/exports.service.ts
@@ -1,8 +1,8 @@
-// Initializes the `users` service on path `/users`
+// Initializes the `exports` service on path `/exports`
 import { ServiceAddons } from '@feathersjs/feathers';
 import { Application } from '../../declarations';
-import Exports from './exports.class';
-import createModel from '../../models/users.model';
+import { Exports } from './exports.class';
+import hooks from './exports.hooks';
 
 // Add this service to the service type index
 declare module '../../declarations' {
@@ -13,10 +13,14 @@ declare module '../../declarations' {
 
 export default function (app: Application): void {
 	const options = {
-		Model: createModel(app),
 		paginate: app.get('paginate'),
 	};
 
 	// Initialize our service with any options it requires
 	app.use('exports', new Exports(options, app));
+
+	// Get our initialized service so that we can register hooks
+	const service = app.service('exports');
+
+	service.hooks(hooks);
 }
