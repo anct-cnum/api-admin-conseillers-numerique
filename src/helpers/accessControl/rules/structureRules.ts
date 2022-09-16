8d8dcba5670fb596c2dcb0b0a8cc91bfdfff13b1
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-09-16 12:24:42 +0200

    ajout nouvelle route pour récupérer une structure

--- src/helpers/accessControl/rules/structureRules.ts
+++ src/helpers/accessControl/rules/structureRules.ts
@@ -3,7 +3,7 @@ import { IUser } from '../../../ts/interfaces/db.interfaces';
 
 export default function structureRules(user: IUser, can) {
   // Restreindre les permissions : les structures ne peuvent voir que les informations les concernant
-  can([action.read], ressource.structures, {
+  can([action.read, action.update], ressource.structures, {
     _id: user?.entity.oid,
   });
   can([action.read, action.update], ressource.users, {
--- src/services/structures/structures.class.ts
+++ src/services/structures/structures.class.ts
@@ -4,6 +4,7 @@ import { Application } from '../../declarations';
 import getStructures from './controllers/getStructures';
 import updateStructure from './controllers/updateStructure';
 import createAbilities from '../../middleware/createAbilities';
+import getStructureById from './controllers/getStructureById';
 
 export default class Structures extends Service {
   constructor(options: Partial<MongooseServiceOptions>, app: Application) {
@@ -13,6 +14,12 @@ export default class Structures extends Service {
       authenticate('jwt'),
       createAbilities,
       getStructures(app),
+    );
+    app.get(
+      '/structure/:id',
+      authenticate('jwt'),
+      createAbilities,
+      getStructureById(app),
     );
     app.patch(
       '/structure/:id',
