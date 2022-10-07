cbd2f2f807943f7adf860c4c5d622e47a92ebebe
dienamo <46058257+dienamo@users.noreply.github.com>
2022-10-07 11:54:32 +0200

    Update src/helpers/auth/createTokens.ts
    Co-authored-by: Sébastien Quirrion <77339872+squirrion@users.noreply.github.com>

--- src/helpers/auth/createTokens.ts
+++ src/helpers/auth/createTokens.ts
@@ -3,7 +3,7 @@ import { IUser } from '../../ts/interfaces/db.interfaces';
 import { Application } from '../../declarations';
 
 const createRefreshToken = (app: Application) => async (user: IUser) => {
-  return sign(user.toJSON(), app.get('inclusionConnect').refreshTokenSecret, {
+  return sign(user.toJSON(), app.get('inclusionConnect').refresh_token_secret, {
     expiresIn: app.get('inclusion_connect').refresh_token_duration,
   });
 };
