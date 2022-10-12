bb9d6d630811aa92224aa9350725db4fd889d8d0
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-10-12 17:21:00 +0200

    modif schema pour l'export des conseillers

--- src/schemas/conseillers.schemas.ts
+++ src/schemas/conseillers.schemas.ts
@@ -36,11 +36,16 @@ const validExportConseillers = Joi.object({
   nomOrdre: Joi.string()
     .required()
     .error(new Error('Nom du sort est invalide')),
-  search: Joi.string().error(new Error('Le search est invalide')),
-  isCoordinateur: Joi.string().error(
+  searchByConseiller: Joi.string().error(
+    new Error('Le search by conseiller est invalide'),
+  ),
+  searchByStructure: Joi.string().error(
+    new Error('Le search by structure est invalide'),
+  ),
+  coordinateur: Joi.string().error(
     new Error('Le filtre coordinateur est invalide'),
   ),
-  isRupture: Joi.string().error(new Error('Le filtre rupture est invalide')),
+  rupture: Joi.string().error(new Error('Le filtre rupture est invalide')),
   region: Joi.string().error(new Error('Le filtre region est invalide')),
 });
 
