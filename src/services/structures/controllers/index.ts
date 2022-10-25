519d9c2c2bfa9a3afeaba6a18c91cb411ef68523
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-10-25 12:00:34 +0200

    supp element inutile

--- src/services/structures/controllers/index.ts
+++ src/services/structures/controllers/index.ts
@@ -1,6 +1,5 @@
 import getStructures from './getStructures';
-import getStructure from './getStructure';
 import updateStructure from './updateStructure';
 
 // eslint-disable-next-line import/prefer-default-export
-export { getStructures, getStructure, updateStructure };
+export { getStructures, updateStructure };
