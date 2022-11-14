a83f59bffb0460bc600a70956eabf1ff5e0da615
Sébastien Quirrion <sebastien@fitlab.fr>
2022-11-14 20:21:59 +0100

    Fix Lint

--- src/services/conseillers/controllers/getCandidatCV.ts
+++ src/services/conseillers/controllers/getCandidatCV.ts
@@ -1,7 +1,7 @@
 import { Application } from '@feathersjs/express';
 import { Response } from 'express';
 import { ObjectId } from 'mongodb';
-import { NotFound, Forbidden, GeneralError } from '@feathersjs/errors';
+import { NotFound, Forbidden } from '@feathersjs/errors';
 import aws from 'aws-sdk';
 import crypto from 'crypto';
 import { IRequest } from '../../../ts/interfaces/global.interfaces';
