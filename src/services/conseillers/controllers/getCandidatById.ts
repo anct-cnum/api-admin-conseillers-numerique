9732dc1a5816006a265d38ef80d10e58618aa813
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-01-12 15:09:48 +0100

    ajout route candidat detail pour structure

--- src/services/conseillers/controllers/getCandidatById.ts
+++ src/services/conseillers/controllers/getCandidatById.ts
@@ -3,15 +3,16 @@ import { Response } from 'express';
 import { ObjectId } from 'mongodb';
 import { IRequest } from '../../../ts/interfaces/global.interfaces';
 import service from '../../../helpers/services';
+import { action } from '../../../helpers/accessControl/accessList';
 
 const getCandidatById =
   (app: Application) => async (req: IRequest, res: Response) => {
     const idConseiller = req.params.id;
     try {
-      // Attention : pas d'access control car tout le monde peut voir tous les candidats
       const conseiller = await app
         .service(service.conseillers)
-        .Model.findOne({ _id: new ObjectId(idConseiller) });
+        .Model.accessibleBy(req.ability, action.read)
+        .findOne({ _id: new ObjectId(idConseiller) });
 
       if (!conseiller) {
         res.status(404).json({ message: 'Conseiller non trouvé' });
