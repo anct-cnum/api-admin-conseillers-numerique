d9d40ab48c466f5dbafbd65b8000b726a783473d
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-03-02 11:21:51 +0200

    Revert "test pour trouver l'erreur 404 not found"
    This reverts commit 4a49cb465f7b054ba7a75d92b7386df01715ba41.

--- src/app.ts
+++ src/app.ts
@@ -17,11 +17,6 @@ import mongoose from './mongoose';
 
 const app = express(feathers());
 
-app.use('/', (req, res) => {
-  logger.info(req.originalUrl);
-  res.status(404).send("Désoler, cette page n'existe pas");
-});
-
 // Init Sentry
 const config = configuration();
 if (config().sentry.enabled === 'true') {
