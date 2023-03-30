59cc7a5de08ac5eb4d166c1741ca3c0493316f81
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-03-30 11:05:53 +0300

    supp element inutile as string

--- src/services/stats/controllers/getDatasStructures.ts
+++ src/services/stats/controllers/getDatasStructures.ts
@@ -16,8 +16,8 @@ import {
 const getDatasStructures =
   (app: Application, options) => async (req: IRequest, res: Response) => {
     try {
-      const dateDebut = new Date(String(req.query.dateDebut));
-      const dateFin = new Date(String(req.query.dateFin));
+      const dateDebut = new Date(req.query.dateDebut);
+      const dateFin = new Date(req.query.dateFin);
       const page = Number(req.query.page);
 
       const items = {
--- src/services/stats/controllers/getStatsStructure.ts
+++ src/services/stats/controllers/getStatsStructure.ts
@@ -12,9 +12,9 @@ const getStatsStructure =
   (app: Application) => async (req: IRequest, res: Response) => {
     try {
       const idStructure = String(req.query?.idStructure);
-      const dateDebut = new Date(String(req.query.dateDebut));
+      const dateDebut = new Date(req.query.dateDebut);
       dateDebut.setUTCHours(0, 0, 0, 0);
-      const dateFin = new Date(String(req.query.dateFin));
+      const dateFin = new Date(req.query.dateFin);
       dateFin.setUTCHours(23, 59, 59, 59);
       const { codePostal, ville } = req.query;
       const statsValidation = validStatStructure.validate({
