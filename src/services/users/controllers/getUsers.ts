9150cddbb50893adf3f29d6660b76fe943b0921c
dienamo <46058257+dienamo@users.noreply.github.com>
2023-04-05 18:01:34 +0200

    informations manquantes conventionnement

--- src/services/users/controllers/getUsers.ts
+++ src/services/users/controllers/getUsers.ts
@@ -12,7 +12,7 @@ const getUsers = (app: Application) => async (req: IRequest, res: Response) => {
       .service(service.users)
       .Model.accessibleBy(req.ability, action.read)
       .find({ _id: { $ne: new ObjectId(req.user?._id) } })
-      .select({ name: 1, passwordCreated: 1 ,sub:1 });
+      .select({ name: 1, passwordCreated: 1 , sub:1 });
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
