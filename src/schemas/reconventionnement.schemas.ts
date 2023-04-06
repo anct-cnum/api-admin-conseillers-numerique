7f445fd0cb740aec77d5e831201a95be7c39fbd2
dienamo <46058257+dienamo@users.noreply.github.com>
2023-04-06 10:02:25 +0200

    fix annulation

--- src/schemas/reconventionnement.schemas.ts
+++ src/schemas/reconventionnement.schemas.ts
@@ -28,8 +28,9 @@ const updateReconventionnement = Joi.object({
   structureId: Joi.string()
     .required()
     .error(new Error("L'identifiant de structure est invalide")),
-  nombreDePostes: Joi.string()
-    .error(new Error('Le nombre de postes est invalide')),
+  nombreDePostes: Joi.string().error(
+    new Error('Le nombre de postes est invalide'),
+  ),
   motif: Joi.string().allow(null, '').error(new Error('Le motif est invalide')),
   conseillers: Joi.array()
     .items(Joi.object())
