52d6c1fc17f00d83cc221714499e0698fafc214e
Ornella Ourfi <ornella93@outlook.fr>
2023-05-22 16:13:05 +0200

    lint

--- src/services/stats/stats.repository.ts
+++ src/services/stats/stats.repository.ts
@@ -581,19 +581,36 @@ const getStatsReorientations = async (query, ability, read, app) => {
     { $project: { _id: 0, organismes: '$_id' } },
   ]);
 
-  let reorientations = [];
+  const reorientations = [];
   let totalReorientations = 0;
-  statsReorientations.forEach(statsReorientation => {
-    if (reorientations.filter(reorientation => reorientation.nom === String(Object.keys(statsReorientation.organismes)[0]))?.length > 0) {
-      reorientations.filter(reorientation => reorientation.nom === Object.keys(statsReorientation.organismes)[0])[0].valeur +=
-        statsReorientation.organismes[Object.keys(statsReorientation.organismes)[0]];
+  statsReorientations.forEach((statsReorientation) => {
+    if (
+      reorientations.filter(
+        (reorientation) =>
+          reorientation.nom ===
+          String(Object.keys(statsReorientation.organismes)[0]),
+      )?.length > 0
+    ) {
+      reorientations.filter(
+        (reorientation) =>
+          reorientation.nom === Object.keys(statsReorientation.organismes)[0],
+      )[0].valeur +=
+        statsReorientation.organismes[
+          Object.keys(statsReorientation.organismes)[0]
+        ];
     } else {
       reorientations.push({
         nom: String(Object.keys(statsReorientation.organismes)[0]),
-        valeur: statsReorientation.organismes[Object.keys(statsReorientation.organismes)[0]]
+        valeur:
+          statsReorientation.organismes[
+            Object.keys(statsReorientation.organismes)[0]
+          ],
       });
     }
-    totalReorientations += statsReorientation.organismes[Object.keys(statsReorientation.organismes)[0]];
+    totalReorientations +=
+      statsReorientation.organismes[
+        Object.keys(statsReorientation.organismes)[0]
+      ];
   });
 
   // Conversion en % total
