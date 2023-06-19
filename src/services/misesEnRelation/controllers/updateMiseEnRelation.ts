42df3672ab9a792c3344b0fec0f07ce6a7175b1f
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-06-19 10:15:13 +0200

    fix: oubli statut pour le recrutement d'un candidat

--- src/services/misesEnRelation/controllers/updateMiseEnRelation.ts
+++ src/services/misesEnRelation/controllers/updateMiseEnRelation.ts
@@ -52,7 +52,7 @@ const updateMiseEnRelation =
             .Model.accessibleBy(req.ability, action.read)
             .find({
               query: {
-                statut: { $in: ['recrutee', 'finalisee'] },
+                statut: { $in: ['recrutee', 'finalisee', 'nouvelle_rupture'] },
               },
             });
           if (
