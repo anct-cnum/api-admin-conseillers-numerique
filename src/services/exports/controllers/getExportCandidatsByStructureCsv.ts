435ed806fd9252089090332414cbbfeacb4e3e76
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-08-02 09:22:55 +0200

    fix: résolution du problème de RAM sur l'export

--- src/services/exports/controllers/getExportCandidatsByStructureCsv.ts
+++ src/services/exports/controllers/getExportCandidatsByStructureCsv.ts
@@ -2,23 +2,43 @@ import { Application } from '@feathersjs/express';
 import { Response } from 'express';
 import { IRequest } from '../../../ts/interfaces/global.interfaces';
 import { IMisesEnRelation } from '../../../ts/interfaces/db.interfaces';
-import { action } from '../../../helpers/accessControl/accessList';
 import service from '../../../helpers/services';
 import { generateCsvCandidatByStructure } from '../exports.repository';
+import { checkAccessReadRequestMisesEnRelation } from '../../misesEnRelation/misesEnRelation.repository';
 
 const getExportCandidatsByStructureCsv =
   (app: Application) => async (req: IRequest, res: Response) => {
     let misesEnRelation: IMisesEnRelation[];
 
     try {
+      const checkAccesMisesEnRelation =
+        await checkAccessReadRequestMisesEnRelation(app, req);
       misesEnRelation = await app
         .service(service.misesEnRelation)
-        .Model.accessibleBy(req.ability, action.read)
-        .find({
-          statut: { $nin: ['finalisee_non_disponible', 'non_disponible'] },
-        })
-        .collation({ locale: 'fr' })
-        .sort({ 'conseillerObj.nom': 1, 'conseillerObj.prenom': 1 });
+        .Model.aggregate(
+          [
+            {
+              $match: {
+                $and: [checkAccesMisesEnRelation],
+                statut: {
+                  $in: ['nouvelle', 'recrutee', 'nonInteressee', 'interessee'],
+                },
+              },
+            },
+            {
+              $project: {
+                conseillerObj: 1,
+              },
+            },
+            {
+              $sort: { 'conseillerObj.nom': 1, 'conseillerObj.prenom': 1 },
+            },
+          ],
+          {
+            collation: { locale: 'fr' },
+            allowDiskUse: true,
+          },
+        );
       generateCsvCandidatByStructure(misesEnRelation, res, app);
     } catch (error) {
       if (error.name === 'ForbiddenError') {
