b94adfc387fe223ccc26eee534ab006d58859475
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-08-03 14:35:14 +0200

    fix: wording script bash incorrect

--- src/tools/jobs/onboardingConseiller.sh
+++ src/tools/jobs/onboardingConseiller.sh
@@ -2,6 +2,6 @@
 
 cd ${APP_HOME}
 
-echo "Création des boîtes email et envoi des emails d'onbording aux conseillers: START\n"
-node_modules/.bin/ts-node src/tools/conseillers/onbordingConseiller.ts
-echo "Création des boîtes email et envoi des emails d'onbording aux conseillers: END\n"
+echo "Création des boîtes email et envoi des emails d'onboarding aux conseillers: START\n"
+node_modules/.bin/ts-node src/tools/conseillers/onboardingConseiller.ts
+echo "Création des boîtes email et envoi des emails d'onboarding aux conseillers: END\n"
