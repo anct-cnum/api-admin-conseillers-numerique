f789c4eb23b40fbcc5fd4bc5ab580220db401f0e
Ornella Ourfi <ornella93@outlook.fr>
2023-09-12 12:07:26 +0200

    2 scripts + rectif commentaire autre script

---
+++ src/tools/scripts/conseillerRecruterPhase.ts
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import { ObjectId } from 'mongodb';
+import execute from '../utils';
+import service from '../../helpers/services';
+import { PhaseConventionnement } from '../../ts/enum';
+
+program.option('-c, --conseillerId <conseillerId>', 'id conseiller');
+program.option('-s, --structureId <structureId>', 'id structure');
+program.option('-p, --phase <phase>', 'phase 1 ou 2');
+program.parse(process.argv);
+
+// ts-node src/tools/scripts/conseillerRecruterPhase2.ts -c XXX -s XXX -p X
+
+execute(__filename, async ({ app, logger, exit }) => {
+  const options = program.opts();
+  if (
+    !ObjectId.isValid(options.structureId) ||
+    !ObjectId.isValid(options.conseillerId) ||
+    !options.phase
+  ) {
+    logger.error(
+      `Veuillez renseigner un id conseiller & un id structure valide & et la phase précise (1 ou 2)`,
+    );
+    return;
+  }
+  let queryUpdate = {};
+  const conseillerId = new ObjectId(options.conseillerId);
+  const structureId = new ObjectId(options.structureId);
+  if (options.phase === '2') {
+    queryUpdate = {
+      $set: {
+        phaseConventionnement: PhaseConventionnement[`PHASE_${options.phase}`],
+      },
+    };
+  } else if (options.phase === '1') {
+    queryUpdate = {
+      $unset: {
+        phaseConventionnement: '',
+      },
+    };
+  } else {
+    logger.error(
+      `Veuillez renseigner une phase valide : ${Object.values(
+        PhaseConventionnement,
+      )} `,
+    );
+    return;
+  }
+  const structure = await app
+    .service(service.structures)
+    .Model.findOne({ _id: structureId });
+  if (structure?.conventionnement?.statut !== 'RECONVENTIONNEMENT_VALIDÉ') {
+    logger.error(
+      `La structure ${structure?.nom} a un statut : ${structure?.conventionnement?.statut}. Le statut doit etre égale à RECONVENTIONNEMENT_VALIDÉ`,
+    );
+    return;
+  }
+  const update = await app.service(service.misesEnRelation).Model.updateOne(
+    {
+      'conseiller.$id': conseillerId,
+      statut: 'finalisee',
+      'structure.$id': structureId,
+    },
+    queryUpdate,
+  );
+  if (update.matchedCount === 1) {
+    logger.info(
+      `Le contrat du conseiller ${conseillerId} est de phase ${options.phase} (structure: ${structure?.nom})`,
+    );
+  } else {
+    logger.info(`Contrat non trouvé.`);
+  }
+  exit();
+});
---
+++ src/tools/scripts/reconventionnementStructure.ts
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import { ObjectId } from 'mongodb';
+import execute from '../utils';
+import service from '../../helpers/services';
+import { StatutConventionnement } from '../../ts/enum';
+
+program.option('-s, --structureId <structureId>', 'id structure');
+program.option('-st, --statut <statut>', 'statut');
+program.parse(process.argv);
+
+// ts-node src/tools/scripts/reconventionnementStructure.ts -s XXX -st XXX
+
+execute(__filename, async ({ app, logger, exit }) => {
+  const options = program.opts();
+  const valueConvention = Object.values(StatutConventionnement).filter((i) =>
+    i.match(/RECONVENTIONNEMENT/g),
+  );
+  if (!options.structureId || !ObjectId.isValid(options.structureId)) {
+    logger.error(`Veuillez renseigner un id structure.`);
+    return;
+  }
+
+  if (!valueConvention.includes(options.statut)) {
+    logger.error(
+      `Veuillez saisir un statut de reconventionnement, parmis la liste : ${valueConvention}`,
+    );
+    return;
+  }
+  const structureId = new ObjectId(options.structureId);
+
+  const update = await app.service(service.structures).Model.updateOne(
+    { _id: structureId },
+    {
+      $set: {
+        'conventionnement.statut': options.statut,
+      },
+    },
+  );
+  if (update.modifiedCount === 1) {
+    await app.service(service.misesEnRelation).Model.updateMany(
+      { 'structure.$id': structureId },
+      {
+        $set: {
+          'structureObj.conventionnement.statut': options.statut,
+        },
+      },
+    );
+    logger.info(
+      `Modification éffectué pour la structure ${structureId} en statut ${options.statut}`,
+    );
+  } else {
+    logger.info(`Structure non trouvé.`);
+  }
+  exit();
+});
