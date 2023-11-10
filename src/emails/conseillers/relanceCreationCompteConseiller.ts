9a670f8e49f5f47e7ce3fe6606626919fe9272a3
morgan <morgan@fitlab.fr>
2023-11-10 17:17:23 +0100

    Wording

--- src/emails/conseillers/relanceCreationCompteConseiller.ts
+++ src/emails/conseillers/relanceCreationCompteConseiller.ts
@@ -56,7 +56,7 @@ export default function (app: Application, mailer, req: IRequest) {
         .createMailer()
         .sendEmail(user.name, {
           subject:
-            'Veuillez activer votre compte Coop des Conseillers numériques France Services',
+            'Veuillez activer votre compte Coop des conseillers numériques',
           body: await render(user),
         })
         .then(onSuccess)
