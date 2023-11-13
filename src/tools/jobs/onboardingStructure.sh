caea02537ecce1e9a413d90b53ab1568b8f4604f
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-11-13 09:33:34 +0100

    fix: correction du path

--- src/tools/jobs/onboardingStructure.sh
+++ src/tools/jobs/onboardingStructure.sh
@@ -3,6 +3,6 @@
 cd ${APP_HOME}
 
 echo "Onboarding des nouvelles structures sur le TDP: START\n"
-node_modules/.bin/ts-node src/tools/migration/send-mail-invitations.ts --role structure --limit 50
+node_modules/.bin/ts-node src/tools/migration/send-mails-invitations.ts --role structure --limit 50
 node_modules/.bin/ts-node src/tools/structures/onboardingStructure.ts
 echo "Onboarding des nouvelles structures sur le TDP: END\n"
