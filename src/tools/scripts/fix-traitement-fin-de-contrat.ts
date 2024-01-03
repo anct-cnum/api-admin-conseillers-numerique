9fed1989085013dec173a397cb41e5e5f48cd7e9
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2024-01-03 18:02:54 +0100

    feat: ajout script de rattrapage

---
+++ src/tools/scripts/fix-traitement-fin-de-contrat.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+/* eslint-disable no-await-in-loop */
+
+// Lancement de ce script : ts-node src/tools/scripts/fix-traitement-fin-de-contrat.ts
+
+import execute from '../utils';
+import service from '../../helpers/services';
+import { PhaseConventionnement } from '../../ts/enum';
+
+execute(__filename, async ({ app, logger, exit }) => {
+  try {
+    const countMiseEnRelationUpdated = await app
+      .service(service.misesEnRelation)
+      .Model.updateMany(
+        {
+          statut: 'finalisee',
+          miseEnRelationConventionnement: { $exists: true },
+          phaseConventionnement: PhaseConventionnement.PHASE_2,
+        },
+        {
+          $unset: {
+            reconventionnement: '',
+          },
+        },
+      );
+    if (countMiseEnRelationUpdated.modifiedCount > 0) {
+      logger.info(
+        `Nombre de mises en relation mises à jour : ${countMiseEnRelationUpdated.modifiedCount}`,
+      );
+    } else {
+      logger.info(`Aucune mise en relation à mettre à jour.`);
+    }
+  } catch (e) {
+    logger.error(e);
+  }
+  exit(0, 'Migration terminée');
+});
