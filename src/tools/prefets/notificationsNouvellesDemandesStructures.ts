780c996d25f6a307ecddd5fc94fe316d424c9211
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2024-01-09 15:39:43 +0100

    fix: wording

--- src/tools/prefets/notificationsNouvellesDemandesStructures.ts
+++ src/tools/prefets/notificationsNouvellesDemandesStructures.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable no-await-in-loop */
 
-// Lancement de ce script : ts-node src/tools/prefets/notificationsNouvellesDemandesCoordinateurs.ts
+// Lancement de ce script : ts-node src/tools/prefets/notificationsNouvellesDemandesStructures.ts
 
 import execute from '../utils';
 import service from '../../helpers/services';
