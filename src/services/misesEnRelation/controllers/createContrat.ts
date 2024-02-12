5f57d0113598fab5b7b9c1acf50b34263f0c8604
Ornella Ourfi <ornella93@outlook.fr>
2024-02-12 17:18:40 +0100

    retour commentaire

--- src/services/misesEnRelation/controllers/createContrat.ts
+++ src/services/misesEnRelation/controllers/createContrat.ts
@@ -52,16 +52,12 @@ const createContrat =
       }
       if (!salaire) {
         delete miseEnRelationObject.salaire;
-      }
-
-      if (salaire) {
-        if (Number(salaire) < Number(app.get('contrat_smic'))) {
-          res.status(400).json({
-            message:
-              'Le salaire doit être égal ou plus élevé que le minimum brut légal',
-          });
-          return;
-        }
+      } else if (Number(salaire) < Number(app.get('contrat_smic'))) {
+        res.status(400).json({
+          message:
+            'Le salaire doit être égal ou plus élevé que le minimum brut légal',
+        });
+        return;
       }
       const miseEnRelationRenouvellementInitiee = await app
         .service(service.misesEnRelation)
