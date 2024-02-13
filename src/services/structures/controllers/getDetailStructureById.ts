1254d43cfec2c99a33c2be68ef451701ccbe017a
moustapha DIENA <mdiena@MacBook-Pro-de-moustapha.local>
2024-02-13 18:51:49 +0100

    ajout du delete mise en relation

--- src/services/structures/controllers/getDetailStructureById.ts
+++ src/services/structures/controllers/getDetailStructureById.ts
@@ -214,6 +214,7 @@ const getDetailStructureById =
           };
         },
       );
+      delete structure[0].misesEnRelations;
 
       const conseillersValiderConventionnement = getConseillersByStatus(
         conseillers,
@@ -291,7 +292,6 @@ const getDetailStructureById =
       )(structure[0]._id);
       structure[0].craCount = craCount;
       structure[0].accompagnementCount = accompagnementsCount[0]?.total;
-      delete structure[0].misesEnRelations;
 
       if (structure.length === 0) {
         return res.status(404).json({ message: 'Structure non trouvée' });
