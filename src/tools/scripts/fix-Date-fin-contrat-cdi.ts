2707359f7ac01cfa8d24beb80e680228419f994f
Ornella Ourfi <ornella93@outlook.fr>
2024-03-01 14:56:25 +0100

    script unset des dates de fin contrat (uniquement cdi)

---
+++ src/tools/scripts/fix-Date-fin-contrat-cdi.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// ts-node src/tools/scripts/fix-Date-fin-contrat-cdi.ts
+
+import execute from '../utils';
+import service from '../../helpers/services';
+
+execute(__filename, async ({ app, logger, exit }) => {
+  const updateMisesEnRelationTypeCdi = await app
+    .service(service.misesEnRelation)
+    .Model.updateMany(
+      {
+        typeDeContrat: 'CDI',
+        dateFinDeContrat: { $exists: true },
+      },
+      {
+        $unset: {
+          dateFinDeContrat: '',
+        },
+      },
+    );
+  logger.info(
+    `${updateMisesEnRelationTypeCdi.modifiedCount} misesEnRelations de type CDI ont eu la date de fin de contrat effacé`,
+  );
+  exit();
+});
