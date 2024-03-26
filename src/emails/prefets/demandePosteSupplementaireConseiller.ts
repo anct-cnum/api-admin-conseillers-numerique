e1c32845d2413387eb987584f27eb9ad07e4e45b
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2024-03-26 14:47:55 +0100

    feat: envoi mail lors de la création d'un avenant

--- src/emails/prefets/demandePosteSupplementaireConseiller.ts
+++ src/emails/prefets/demandePosteSupplementaireConseiller.ts
@@ -1,53 +1,108 @@
 import { Application } from '@feathersjs/express';
+import { ObjectId } from 'mongodb';
 import service from '../../helpers/services';
 import logger from '../../logger';
+import { IStructures } from '../../ts/interfaces/db.interfaces';
 
 export default function (app: Application, mailer) {
-  const templateName = 'informationNouvelleCandidatureConseiller';
+  const templateName = 'demandePosteSupplementaireConseiller';
   const { utils } = mailer;
 
   const render = async (nomStructure: string) => {
     return mailer.render(__dirname, templateName, {
       nomStructure,
       mail: app.get('smtp').replyTo,
-      pilotage: utils.utils.getDashboardUrl('/'),
-      pilotagePrefet: utils.utils.getDashboardUrl(
-        '/prefet/demandes/conseillers',
-      ),
+      pilotage: utils.getDashboardUrl('/'),
+      pilotagePrefet: utils.getDashboardUrl('/prefet/demandes/conseillers'),
     });
   };
 
   return {
     render,
-    send: async (prefetWithStructure) => {
+    send: async (
+      mailPrefet: string,
+      structure: IStructures,
+      idDemandeCoselec: ObjectId,
+    ) => {
       const onSuccess = async () => {
         await app.service(service.structures).Model.updateOne(
           {
-            _id: prefetWithStructure.idStructure,
+            _id: structure._id,
+            demandesCoselec: {
+              $elemMatch: {
+                id: idDemandeCoselec,
+              },
+            },
           },
           {
             $set: {
-              'prefet.MailDateDemandePosteSupplementaire': new Date(),
+              'demandesCoselec.$.prefet.mailSendDatePrefet': new Date(),
             },
             $unset: {
-              'prefet.mailErrorSentDateDemandePosteSupplementaire': '',
-              'prefet.mailErrorDetailSentDateDemandePosteSupplementaire': '',
+              'demandesCoselec.$.prefet.mailErrorSendDatePrefet': '',
+              'demandesCoselec.$.prefet.mailErrorDetailSendDatePrefet': '',
+            },
+          },
+        );
+        await app.service(service.misesEnRelation).Model.updateMany(
+          {
+            'structure.$id': structure._id,
+            'structureObj.demandesCoselec': {
+              $elemMatch: {
+                id: idDemandeCoselec,
+              },
+            },
+          },
+          {
+            $set: {
+              'structureObj.demandesCoselec.$.prefet.mailSendDatePrefet':
+                new Date(),
+            },
+            $unset: {
+              'structureObj.demandesCoselec.$.prefet.mailErrorSendDatePrefet':
+                '',
+              'structureObj.demandesCoselec.$.prefet.mailErrorDetailSendDatePrefet':
+                '',
             },
           },
         );
         logger.info(
-          `Email envoyé avec succès pour l'information d'une demande de poste(s) supplémentaire(s) au préfet ${prefetWithStructure.name}`,
+          `Email envoyé avec succès pour l'information d'une demande de poste(s) supplémentaire(s) au préfet ${mailPrefet}`,
         );
       };
       const onError = async (err: Error) => {
         await app.service(service.structures).Model.updateOne(
           {
-            _id: prefetWithStructure.idStructure,
+            _id: structure._id,
+            demandesCoselec: {
+              $elemMatch: {
+                id: idDemandeCoselec,
+              },
+            },
           },
           {
-            'prefet.mailErrorSentDateDemandePosteSupplementaire': 'smtpError',
-            'prefet.mailErrorDetailSentDateDemandePosteSupplementaire':
-              err.message,
+            $set: {
+              'demandesCoselec.$.mailErrorSendDatePrefet': 'smtpError',
+              'demandesCoselec.$.mailErrorDetailSendDatePrefet': err.message,
+            },
+          },
+        );
+        await app.service(service.misesEnRelation).Model.updateMany(
+          {
+            'structure.$id': structure._id,
+            'structureObj.demandesCoselec': {
+              $elemMatch: {
+                id: idDemandeCoselec,
+              },
+            },
+          },
+          {
+            $set: {
+              'structureObj.demandesCoselec.$.prefet.mailErrorSendDatePrefet':
+                'smtpError',
+              'structureObj.demandesCoselec.$.prefet.mailErrorDetailSendDatePrefet':
+                err.message,
+            },
           },
         );
         throw err;
@@ -55,10 +110,10 @@ export default function (app: Application, mailer) {
 
       return mailer
         .createMailer()
-        .sendEmail(prefetWithStructure.name, {
+        .sendEmail(mailPrefet, {
           subject:
             'Conseiller numérique - demande de poste(s) supplémentaire(s)',
-          body: await render(prefetWithStructure.structure.nom),
+          body: await render(structure.nom),
         })
         .then(onSuccess)
         .catch(onError);
