fdfc80b79008a94b95ae8ce33efe07a70908a4cb
Ornella Ourfi <ornella93@outlook.fr>
2024-03-27 15:31:03 +0100

    ajout 2 colonnes in export cra M+2

--- src/services/exports/exports.repository.ts
+++ src/services/exports/exports.repository.ts
@@ -284,6 +284,7 @@ const generateCsvConseillersWithoutCRA = async (
 ) => {
   try {
     const fileHeaders = [
+      "id Conum",
       'Nom',
       'Prénom',
       'Email @conseiller-numerique.fr',
@@ -291,7 +292,8 @@ const generateCsvConseillersWithoutCRA = async (
       'Code département du conseiller',
       'Numéro de téléphone du conseiller',
       "Date d'envoi du mail M+1",
-      "Date d'envoi du mail M+1,5",
+      "Date d'envoi du mail M+1.5",
+      "Groupe CRA",
       'Id de la structure',
       'Siret de la structure',
       'Nom de la structure',
@@ -303,6 +305,7 @@ const generateCsvConseillersWithoutCRA = async (
         fileHeaders.join(csvCellSeparator),
         ...conseillers.map((statCnfsWithoutCRA) =>
           [
+            statCnfsWithoutCRA.idPG,
             statCnfsWithoutCRA.nom,
             statCnfsWithoutCRA.prenom,
             statCnfsWithoutCRA.emailCN?.address,
@@ -320,6 +323,12 @@ const generateCsvConseillersWithoutCRA = async (
                 -1,
                 statCnfsWithoutCRA.groupeCRAHistorique,
               )[0]['dateMailSendConseillerM+1,5'],
+            ),
+            formatDate(
+              getFormatHistoriqueGroupeCRA(
+                -1,
+                statCnfsWithoutCRA.groupeCRAHistorique,
+              )[0].numero,
             ),
             statCnfsWithoutCRA.structure.idPG,
             statCnfsWithoutCRA.structure.siret,
