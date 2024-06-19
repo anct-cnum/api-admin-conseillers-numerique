4e756e3e7bc697ff68861b78b3539085da544229
Fabien Mercier <fabien.mercier@octo.com>
2024-06-19 19:43:37 +0200

    Suppression des tests inutiles

--- test/services/conseillers.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../src/app';
-
-describe("'conseillers' service", () => {
-  it('registered the service', () => {
-    const service = app.service('conseillers');
-    expect(service).toBeTruthy();
-  });
-});
--- test/services/structures.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../src/app';
-
-describe("'structures' service", () => {
-  it('registered the service', () => {
-    const service = app.service('structures');
-    expect(service).toBeTruthy();
-  });
-});
