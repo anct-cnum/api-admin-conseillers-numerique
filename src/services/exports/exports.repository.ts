790e8a8c911575b019ea1072fb21038771487c7f
Ornella Ourfi <ornella93@outlook.fr>
2024-07-10 09:42:08 +0200

    2 nouvelles colonnes 'Date Coselec;Phase en cours' dans l'export Fichier structures

--- src/services/exports/exports.repository.ts
+++ src/services/exports/exports.repository.ts
@@ -5,7 +5,7 @@ import utc from 'dayjs/plugin/utc';
 import { Response } from 'express';
 import { Application } from '@feathersjs/express';
 import { ObjectId } from 'mongodb';
-import { formatDateGMT, getCoselec, getCoselecLabel } from '../../utils';
+import { formatDateGMT, getCoselec, getCoselecLabel, getCoselecPositifAvantAbandon } from '../../utils';
 import service from '../../helpers/services';
 import {
   IMisesEnRelation,
@@ -22,6 +22,7 @@ import { formatStatutMisesEnRelation } from '../conseillers/repository/conseille
 import {
   PhaseConventionnement,
   AffichagePhaseConventionnement,
+  StatutConventionnement,
 } from '../../ts/enum';
 
 dayjs.extend(utc);
@@ -320,7 +321,7 @@ const generateCsvStructure = async (
   app: Application,
 ) => {
   res.write(
-    'SIRET structure;ID Structure;Dénomination;Type;Statut;Code postal;Code commune;Code département;Code région;Téléphone;Email;Compte créé;Mot de passe choisi;Nombre de mises en relation;Nombre de conseillers souhaités;Validée en COSELEC;Nombre de conseillers validés par le COSELEC;Numéro COSELEC;ZRR;QPV;Nombre de quartiers QPV;Labelisée France Services;Raison sociale;Nom commune INSEE;Code commune INSEE;Adresse postale;Libellé catégorie juridique niv III;Grand Réseau;Nom Grand Réseau;Emails administrateurs\n',
+    'SIRET structure;ID Structure;Dénomination;Date Coselec;Phase en cours;Type;Statut;Code postal;Code commune;Code département;Code région;Téléphone;Email;Compte créé;Mot de passe choisi;Nombre de mises en relation;Nombre de conseillers souhaités;Validée en COSELEC;Nombre de conseillers validés par le COSELEC;Numéro COSELEC;ZRR;QPV;Nombre de quartiers QPV;Labelisée France Services;Raison sociale;Nom commune INSEE;Code commune INSEE;Adresse postale;Libellé catégorie juridique niv III;Grand Réseau;Nom Grand Réseau;Emails administrateurs\n',
   );
   try {
     await Promise.all(
@@ -338,6 +339,7 @@ const generateCsvStructure = async (
         );
         const coselec = getCoselec(structure);
         let label = 'non renseigné';
+        let phaseConventionnementStructure = getCoselecPositifAvantAbandon(structure) ? AffichagePhaseConventionnement.PHASE_1 : '';
         if (
           structure?.estLabelliseFranceServices &&
           structure.estLabelliseFranceServices === 'OUI'
@@ -349,6 +351,9 @@ const generateCsvStructure = async (
         ) {
           label = 'non';
         }
+        if ([`${StatutConventionnement.RECONVENTIONNEMENT_VALIDÉ}`, `${StatutConventionnement.CONVENTIONNEMENT_VALIDÉ_PHASE_2}`].includes(structure?.conventionnement?.statut)) {
+          phaseConventionnementStructure = AffichagePhaseConventionnement.PHASE_2;
+        } 
         let adresse = `${structure?.insee?.adresse?.numero_voie ?? ''}
         ${structure?.insee?.adresse?.type_voie ?? ''}
         ${structure?.insee?.adresse?.libelle_voie ?? ''}\n${structure?.insee?.adresse?.complement_adresse
@@ -360,7 +365,8 @@ const generateCsvStructure = async (
         adresse = adresse.replace(/["',]/g, '');
 
         res.write(
-          `${structure.siret};${structure.idPG};${structure.nom};${structure.type === 'PRIVATE' ? 'privée' : 'publique'
+          `${structure.siret};${structure.idPG};${structure.nom};${coselec?.insertedAt ? formatDate(coselec.insertedAt) : ''
+          };${phaseConventionnementStructure};${structure.type === 'PRIVATE' ? 'privée' : 'publique'
           };${structure.statut};${structure.codePostal};${structure.codeCommune
           };${structure.codeDepartement};${structure.codeRegion};${structure.contact?.telephone};${structure.contact?.email};${structure.userCreated ? 'oui' : 'non'
           };${userPrincipal?.sub ? 'oui' : 'non'};${countMisesEnRelation};${structure.nombreConseillersSouhaites ?? 0
