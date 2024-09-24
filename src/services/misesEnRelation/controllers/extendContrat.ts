726ce6e6b1d4708b371ec6392636d1067324b9ad
Ornella <68587983+Ornella452@users.noreply.github.com>
2024-09-24 11:48:04 +0200

    fix update salaire (#668)

--- src/services/misesEnRelation/controllers/extendContrat.ts
+++ src/services/misesEnRelation/controllers/extendContrat.ts
@@ -69,6 +69,9 @@ const extendContrat =
                 'demandesDeProlongation.$.dateDeFinSouhaitee': new Date(
                   dateFinDeContrat,
                 ),
+                'demandesDeProlongation.$.salaireActuelle':
+                  miseEnRelation.salaire,
+                'demandesDeProlongation.$.salaireSouhaitee': Number(salaire),
               },
             },
             {
