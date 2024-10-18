d4692425b5bca4f84a5dbf1afa153a056a6e21b1
dienamo <dienamoustapha@gmail.com>
2024-10-18 15:27:11 +0100

    :fix paramétrage du cookie en prod

--- src/services/users/controllers/signIn.ts
+++ src/services/users/controllers/signIn.ts
@@ -195,6 +195,13 @@ const signIn = (app: Application) => async (req: IRequest, res: Response) => {
       // envoi du refresh token dans un cookie
       res.cookie(app.get('pro_connect').refresh_token_key, refreshToken, {
         httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        domain:
+          process.env.NODE_ENV === 'production'
+            ? '.cleverapps.io'
+            : 'localhost',
       });
       // envoi de l'access token
       return res.status(200).json({ user: user._doc, accessToken });
