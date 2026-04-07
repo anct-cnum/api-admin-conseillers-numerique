390b59a370e1e618bef7cb14367d58295512b8f5
ornella452 <ornella93@outlook.fr>
2026-04-07 17:10:23 +0200

    fix: resolve erreur UnhandledRejection

--- src/services/stats/controllers/getStatsNationalesNouvelleCoop.ts
+++ src/services/stats/controllers/getStatsNationalesNouvelleCoop.ts
@@ -98,7 +98,10 @@ const getStatsNationalesNouvelleCoop =
       }
       return res.status(200).json(donneesStats.data);
     } catch (error) {
-      throw new Error(error);
+      if (error.isJoi || error.name === 'ValidationError') {
+        return res.status(400).json({ message: error.message });
+      }
+      return res.status(500).json({ message: error.message });
     }
   };
 
