6f6505039ce5f6d8a9c0befde0f9d34194e75aee
ornella452 <ornella93@outlook.fr>
2026-08-20 17:59:49 +0200

    fix: ferme les endpoints REST (accessLogs, permanences, conseillersSupprimes, conseillersRuptures)

--- src/services/accessLogs/accessLogs.hook.ts
+++ src/services/accessLogs/accessLogs.hook.ts
@@ -1,12 +1,17 @@
+import * as feathersAuthentication from '@feathersjs/authentication';
+import disallowExternal from '../../hooks/disallowExternal';
+
+const { authenticate } = feathersAuthentication.hooks;
+
 export default {
   before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
+    all: [authenticate('jwt')],
+    find: [disallowExternal()],
+    get: [disallowExternal()],
+    create: [disallowExternal()],
+    update: [disallowExternal()],
+    patch: [disallowExternal()],
+    remove: [disallowExternal()],
   },
 
   after: {
